e893084582f598a6111bf6fb5981a84596cb3c86
Nicole-Trumbull <trumbull.mom80@gmail.com>
2023-10-11 10:41:46 -0600

    Push to GitHub Coding Dojo assignment

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeText(element) {
+    console.log(element.innerText);
+    element.innerText = "Log Out"
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function popUp(element) {
+    alert("Ninja was liked");
+}
